be2f5955de1504b4ff809933841fb5ffd9c8ff7f
ritampaine75-debug <ritampaine75@gmail.com>
2026-01-28 10:47:35 +0530

    Add theme colors for primary and secondary elements

---
+++ src/theme/colors.js
@@ -0,0 +1,8 @@
+export const theme = {
+  primary: '#e85d04', // Burnt Orange
+  secondary: '#faa307', // Gold
+  background: '#fff8f2', // Light Cream
+  text: '#370617', // Dark Red/Brown
+  white: '#ffffff',
+  error: '#d00000'
+};
